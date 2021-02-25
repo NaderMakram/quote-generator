deb2c0325d2fa1528927a8555b14a10b4efe2cf8
NaderMakram <nadermakram98@gmail.com>
2021-02-25 16:31:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// selectors
+
+const newQuoteButton = document.querySelector('#new-quote')
+const tweetButton = document.querySelector('#twitter')
+
+const quoteTemplate = document.querySelector('#quote')
+const authorTemplate = document.querySelector('#author')
+
+const container = document.querySelector('#container')
+const loader = document.querySelector('#loader')
+
+
+
+// functions
+
+
+// show and hide loader and quote-container
+function toggleLoader() {
+  loader.hidden = !loader.hidden;
+  container.hidden = !container.hidden;
+}
+
+
+
+async function getQuote() {
+  toggleLoader()
+  try {
+    const res = await fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=1')
+    let data = await res.json()
+    let quote = data.quotes[0].text
+    let author = data.quotes[0].author
+    if (quoteTemplate.textContent == quote) {
+      console.log('---------------- its the same quote, get another')
+      return getQuote()
+    }
+    // if there is no author, add unknown
+    if (author === '') {
+      authorTemplate.textContent = 'Unknown'
+    } else {
+      authorTemplate.textContent = author
+    }
+    // // reduce font size for long quotes
+    if (quote.length > 120) {
+      quoteTemplate.classList.add('long-text')
+    } else {
+      quoteTemplate.classList.remove('long-text')
+    }
+    quoteTemplate.textContent = quote
+  } catch (err) {
+    // getQuote()
+    console.error(err)
+    console.log(err)
+  }
+  toggleLoader()
+}
+
+
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args)
+    }, delay)
+  }
+}
+
+
+
+function tweetQuote() {
+  let quote = quoteTemplate.textContent
+  let author = authorTemplate.textContent
+  let msg = `${quote}\n${author}`
+  let twitterUrl = "https://twitter.com/intent/tweet?text=" + escape(msg)
+  console.log(msg)
+  window.open(twitterUrl, '_blank')
+}
+
+
+// events
+newQuoteButton.addEventListener('click', debounce(getQuote, 500))
+tweetButton.addEventListener('click', tweetQuote)
